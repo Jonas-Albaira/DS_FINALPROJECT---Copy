fa30095970ca06f1099ba7dba2f498f4eb9f0c81
Jonas-Albaira <jonas.albaira@uoit.net>
2018-11-30 10:56:03 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+import os
+
+app = Flask(__name__)
+
+app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
+socketio = SocketIO( app )
+
+users=[" "]*30
+count = 0
+
+#routing/mapping of pages, tie URL to a python function
+#root of website
+# @ symbolizes a decorator
+@app.route('/', methods=['GET','POST'])
+def index():
+    return render_template("index.html")
+
+def messageRecived():
+  print( 'message was received!!!' )
+
+@socketio.on( 'my event' )
+def handle_my_custom_event( json ):
+  print( 'recived my event: ' + str( json ) )
+  socketio.emit( 'my response', json, callback=messageRecived )
+
+@socketio.on( 'username' )
+def receive_username(info):
+    global count
+    users[count]=info
+    print(users)
+    count+=1
+    socketio.emit('display', users)
+
+if __name__ == '__main__':
+  socketio.run( app, host='0.0.0.0', debug=False )
+
